8483cf6678b6789eb8c66712b36451e3f94b07fe
Ashutosh Yadav <ashutoshslife@gmail.com>
2024-07-07 22:14:10 +0530

    chenges in addition file

--- src/com/demo/Addition.java
+++ src/com/demo/Addition.java
@@ -9,7 +9,10 @@ public class Addition {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter 1st no: ");
 		int a = sc.nextInt();
+		System.out.println("Enter 2nd no: ");
 		int b = sc.nextInt();
 		int c = Addition.add(a, b);
 		System.out.println(String.format("The sum of a=%d and b = %d id c=%d", a,b,c));
